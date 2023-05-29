b9e54eedd3470e7b49bd240d2a93b326635dbaa9
Aditya Zope <47198400+a007zope@users.noreply.github.com>
2023-05-29 15:31:12 +0530

    Update LoginPageCommit.java

--- src/Pages/LoginPageCommit.java
+++ src/Pages/LoginPageCommit.java
@@ -6,7 +6,12 @@ public class LoginPageCommit {
 		System.out.println("Dev1LoginCommitStashScenario2");
 		System.out.println("Dev1");
 		System.out.println("localchanges");
-
 	}
+	
+	public void loginpage(String dev2)
+	{
+		String dev2 ="Dev2RemoteChanges";
+         System.out.println();
 
+}
 }
