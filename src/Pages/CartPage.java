6b65dac92e85eaeca1585277be4a5e371464978c
vzopad <aditya.zope@pearson.com>
2023-05-29 06:51:30 +0530

    practicing existing cherrypick in git cherrypickexisting3

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -5,6 +5,18 @@ public class CartPage {
 	public void cart()
 	{
 		System.out.println("CartPage");
+
+		String name ="cartpageSelectedForCheeryPIck";
+		String NameOfPerson ="Aditya123";
+		String surName ="Zope";
+		
+	
+	}
+	public static void main(String[] args) {
+		CartPage c = new CartPage();
+		c.cart();
+		System.out.println("existing cherrypick successful");
+
 	}
 
 }
