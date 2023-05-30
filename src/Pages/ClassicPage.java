97537fb57094f2e3df16949d70f4aa5740a28420
Aditya Zope <47198400+a007zope@users.noreply.github.com>
2023-05-30 06:18:55 +0530

    Create ClassicPage.java

---
+++ src/Pages/ClassicPage.java
@@ -0,0 +1,11 @@
+package Pages;
+public class ClassicPage {	
+	public void classic()
+	{
+		int n = 10;
+		int m = 20;
+		int k = m/n;
+		System.out.println(k);
+    System.out.println("Divsison logic by Remote");
+	}	
+}
