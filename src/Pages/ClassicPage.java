4f7cc4b1791ae19465974bae53a72a3b29d320bb
vzopad <aditya.zope@pearson.com>
2023-05-31 17:37:28 +0530

    removed spaces

--- src/Pages/ClassicPage.java
+++ src/Pages/ClassicPage.java
@@ -8,12 +8,9 @@ public class ClassicPage {
 		int m = 20;
 		int k = m/n;
 		System.out.println(k);
-
 		System.out.println("Divison logic by Remote");
 		System.out.println("Accepted remote dev's logic aftre discussion");
-		
 		String name ="Aditya";
-		
 		int salary = 15;
 		int age = 25;
 		System.out.println("Classic Page of working eclipse IDE");
