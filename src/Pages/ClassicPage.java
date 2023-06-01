18b8a9dbf1d853bb93917bcdd6ef3ef46b9b52b9
vzopad <aditya.zope@pearson.com>
2023-06-01 06:09:36 +0530

    latest commit in classic page

--- src/Pages/ClassicPage.java
+++ src/Pages/ClassicPage.java
@@ -14,7 +14,6 @@ public class ClassicPage {
 		int salary = 15;
 		int age = 25;
 		System.out.println("Classic Page of working eclipse IDE");
-
 		int h = 10;
 		int b =10;
 	}	
@@ -23,6 +22,7 @@ public class ClassicPage {
 	{
 		System.out.println("hi Master test");
 		int noOfCars =2;
+		System.out.println("classic");
 	}
 
 }
